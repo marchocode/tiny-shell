88e52da8bbd6088ee37698dca5d82d661a31ce17
root <root@promote.cache-dns.local>
2022-08-01 16:28:43 +0800

    update

--- update-mirrors.sh
+++ update-mirrors.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-DIR=$(pwd)
-WORK_DIR=${DIR}/backup
-OS=$(lsb_release -si)
-CODE=$(lsb_release -sc)
-SOURCE_FILE='/etc/apt'
-
 # 163 http://mirrors.163.com/debian/
 # huaweicloud https://mirrors.huaweicloud.com/debian/
 # aliyun https://mirrors.aliyun.com/debian/
 
+DIR=$(pwd)
+WORK_DIR=${DIR}/.mirrors
+SOURCE_FILE='/etc/apt/sources.list'
+REMOTE_MIRRORS="https://gitee.com/marchocode/shell/raw"
+OS=`cat /etc/os-release | grep "^ID" | cut -d "=" -f 2`
+VERSION_CODE=`cat /etc/os-release | grep "VERSION_CODENAME" | cut -d "=" -f 2`
+
+echo "1. create work dir"
 mkdir -p ${WORK_DIR}
 
 # backup
-copy ${SOURCE_FILE} ${WORK_DIR}/backup
+echo "2. backup file"
+cp ${SOURCE_FILE} ${WORK_DIR}/sources.old
 
 # download template
-wget -O 
+echo "3. download template"
+wget -P ${WORK_DIR} https://gitee.com/marchocode/shell/raw/master/mirrors/163.debian.sources.list
+
+# make mirrors file
+sed -i 's/code/'${VERSION_CODE}'/g' ${WORK_DIR}/163.debian.sources.list
+sed -i 's/host/mirrors.163.com/g' ${WORK_DIR}/163.debian.sources.list
