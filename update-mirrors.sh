b92f8ffac55270fd5182bb7832c1e319ee6c3335
root <root@promote.cache-dns.local>
2022-08-01 18:03:55 +0800

    support debian

--- update-mirrors.sh
+++ update-mirrors.sh
@@ -5,11 +5,23 @@
 # aliyun https://mirrors.aliyun.com/debian/
 
 DIR=$(pwd)
-WORK_DIR=${DIR}/.mirrors
+HOST="mirrors.aliyun.com"
+WORK_DIR=${DIR}/.backup
 SOURCE_FILE='/etc/apt/sources.list'
-REMOTE_MIRRORS="https://gitee.com/marchocode/shell/raw"
+REMOTE_MIRRORS="https://gitee.com/marchocode/shell/raw/master/mirrors"
 OS=`cat /etc/os-release | grep "^ID" | cut -d "=" -f 2`
 VERSION_CODE=`cat /etc/os-release | grep "VERSION_CODENAME" | cut -d "=" -f 2`
+MIRRORS_FILE=${OS}.source
+
+MIRRORS_LIST=("" "mirrors.163.com" "mirrors.aliyun.com")
+
+echo "请选择系统源："
+echo "1. mirrors.163.com"
+echo "2. mirrors.aliyun.com"
+echo ""
+read -p "请输入[默认2]: " CHECK
+
+HOST=${MIRRORS_LIST[$CHECK]}
 
 echo "1. create work dir"
 mkdir -p ${WORK_DIR}
@@ -20,8 +32,16 @@ cp ${SOURCE_FILE} ${WORK_DIR}/sources.old
 
 # download template
 echo "3. download template"
-wget -P ${WORK_DIR} https://gitee.com/marchocode/shell/raw/master/mirrors/163.debian.sources.list
+wget -P ${WORK_DIR} ${REMOTE_MIRRORS}/${MIRRORS_FILE}
 
 # make mirrors file
-sed -i 's/code/'${VERSION_CODE}'/g' ${WORK_DIR}/163.debian.sources.list
-sed -i 's/host/mirrors.163.com/g' ${WORK_DIR}/163.debian.sources.list
+echo "4. making files"
+
+sed -i 's/code/'${VERSION_CODE}'/g' ${WORK_DIR}/${MIRRORS_FILE}
+sed -i 's/host/'${HOST}'/g' ${WORK_DIR}/${MIRRORS_FILE}
+
+# deploy
+echo "5. Deploy...."
+cp ${WORK_DIR}/${MIRRORS_FILE} ${SOURCE_FILE}
+
+echo "6. Shell is over"
