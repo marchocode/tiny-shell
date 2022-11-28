56b47bf359b2bb1c6c2c6e48d7992fd55a79afbc
Marcho <41415464+marchocode@users.noreply.github.com>
2022-11-28 07:24:06 +0000

    :tada: tiny-shell commit

--- tiny-shell.sh
+++ tiny-shell.sh
@@ -4,23 +4,28 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
+line="----------------------------------------------"
 
 [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
 # check os
+# check code
 
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
 elif cat /etc/issue | grep -Eqi "debian"; then
     release="debian"
+    version=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f 2)
 elif cat /etc/issue | grep -Eqi "ubuntu"; then
     release="ubuntu"
+    version=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f 2)
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 elif cat /proc/version | grep -Eqi "debian"; then
     release="debian"
 elif cat /proc/version | grep -Eqi "ubuntu"; then
     release="ubuntu"
+    version=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f 2)
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
@@ -28,38 +33,69 @@ else
 fi
 
 mirrors(){
-    
+    clear
     echo -e ""
-    echo -e "${green}tiny-shell ${plain}mirrors"
-    echo -e "----------------------------------------------"
+    echo -e "${green}tiny-shell ${plain}System Mirrors Check"
+    echo -e ${line}
     echo -e ""
-    echo -e "1. mirrors.163.com                     - 网易开源镜像站"
-    echo -e "2. mirrors.aliyun.com                  - 阿里云镜像站"
-    echo -e "3. mirrors.ustc.edu.cn                 - 中国科学技术大学开源软件镜像"
-    echo -e "4. mirrors.tuna.tsinghua.edu.cn        - 清华大学开源软件镜像站"
-    echo -e ""
-    echo -e "----------------------------------------------"
 
-    echo -e "${red}请选择编号：默认【1】${plain}" && read CHOISE
-    if [ -z $CHOISE ]; then
-        CHOISE="1"
+    mirrors_list=($(cat host.mirrors))
+    mirrors_length=$(expr ${#mirrors_list[@]} - 1)
+
+    for (( i=0; i<${#mirrors_list[@]}; i++ ));
+    do
+        host=$(echo ${mirrors_list[$i]} | cut -d "|" -f 1)
+        name=$(echo ${mirrors_list[$i]}| cut -d "|" -f 2)
+
+        echo -e "${i}. ${green}${host}${plain}(${name})"
+    done
+
+    echo -e ""
+    echo -e ${line}
+
+    read -p "请输入选择 [0-${mirrors_length}](默认0): " CHOISE
+
+    if [[ -z $CHOISE ]]; then
+        CHOISE="0"
     fi
 
-    echo ${CHOISE}
+    check_host=${mirrors_list[${CHOISE}]}
 
+    host=$(echo ${check_host} | cut -d "|" -f 1)
+    repo_config="./${release}/${version}"
+    destination=$(cat mirrors.destination | grep ${release} | cut -d '|' -f 2)
+    backup=".sources.list.backup"
+
+    echo -e ""
+    echo -e "OS                   - ${release}"
+    echo -e "Version              - ${version}"
+    echo -e "Mirror               - ${check_host}"
+    echo -e "Destination          - ${destination}"
+    echo -e ${line}
+
+    # check release file if exits
+    if [[ ! -e repo_config ]]; then
+        repo_config="./${release}/default"
+    fi
+
+    # file backup
+    cp ${destination} ${backup}
+
+    #sed "s/host/${host}/g" ${repo_config} > ${destination}
+    echo -e "${red}感谢您的使用,系统安装源已经被替换${plain}"
 }
 
 echo -e ""
 echo -e "${green}tiny-shell ${plain}使用帮助："
 
-echo -e "----------------------------------------------"
+echo -e ${line}
 echo -e "${green}tiny-shell ${plain}一个帮助你节省一堆时间的shell脚本"
 echo -e ""
 echo -e "tiny-shell                      - 显示帮助菜单"
 echo -e "tiny-shell mirrors              - 切换国内系统镜像源(阿里云/网易/清华大学)众多镜像站收录"
 echo -e "tiny-shell docker               - 快速安装docker"
 echo -e "tiny-shell pip                  - 快速配置pip加速镜像"
-echo -e "----------------------------------------------"
+echo -e ${line}
 echo -e ""
 
 mirrors
