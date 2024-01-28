9aa39f5edfe3bcbd9005264728e2b3bc183db082
marcho <marchocode@gmail.com>
2024-01-28 08:02:12 +0000

    modify install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 BUCKET="https://tiny-shell.chaobei.xyz"
 curl -s -o tiny-shell.sh ${BUCKET}/tiny-shell.sh
 
-sed 's/file:${pwd}/https:\/\/tiny-shell.chaobei.xyz/g' tiny-shell.sh
+sed -i 's/file:${pwd}/https:\/\/tiny-shell.chaobei.xyz/g' tiny-shell.sh
 chmod +x tiny-shell.sh
 
 ./tiny-shell.sh
